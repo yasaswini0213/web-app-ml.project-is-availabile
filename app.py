0ab72dc04d93c8b4225ce0fdfd0a083b06b85466
yasaswini0213 <155796169+yasaswini0213@users.noreply.github.com>
2024-01-06 11:47:46 +0530

    create ml model

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ import streamlit as st
 
 dataset=pd.read_csv('crop.csv')
 X=dataset.iloc[:,:-1].values
-Y=dataset.iloc[:,:-1].values
+Y=dataset.iloc[:,-1].values
 
 X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2)
 
 classifier=LogisticRegression()
 classifier.fit(X_train,Y_train)
 
-st.title.('Crop Recommendation')
+st.title('Crop Recommendation')
 n=st.number_input('Enter Nitrogen:')
 p=st.number_input('Enter Phosphorous:')
 k=st.number_input('Enter Potassium:')
